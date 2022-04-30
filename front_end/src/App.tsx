ed98da648cdc353a5a150ecfb0071c192b8d4f1e
Ritesh gurjar <gurjarritesh155@gmail.com>
2022-04-30 17:25:22 +0530

    First commit

---
+++ front_end/src/App.tsx
@@ -0,0 +1,22 @@
+import { DAppProvider, ChainId } from "@usedapp/core";
+import { Header } from "./components/Header";
+import { Container } from "@material-ui/core";
+import { Main } from "./components/Main";
+
+function App() {
+  return (
+    <DAppProvider
+      config={{
+        supportedChains: [ChainId.Kovan, ChainId.Rinkeby],
+      }}
+    >
+      <Header />
+      <Container maxWidth="md">
+        <div>Hiii</div>
+        <Main />
+      </Container>
+    </DAppProvider>
+  );
+}
+
+export default App;
